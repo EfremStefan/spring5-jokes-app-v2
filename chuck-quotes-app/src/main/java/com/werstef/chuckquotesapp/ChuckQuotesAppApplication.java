ab79b2076508263575ce0d10550f9613e3b3385e
Werstef <st.efrem98@gmail.com>
2022-08-09 11:39:19 +0300

    added base project

---
+++ chuck-quotes-app/src/main/java/com/werstef/chuckquotesapp/ChuckQuotesAppApplication.java
@@ -0,0 +1,13 @@
+package com.werstef.chuckquotesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckQuotesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuckQuotesAppApplication.class, args);
+	}
+
+}
---
+++ chuck-quotes-app/src/test/java/com/werstef/chuckquotesapp/ChuckQuotesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.werstef.chuckquotesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckQuotesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
